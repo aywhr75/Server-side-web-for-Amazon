2db19392a4f434487daa1a1154cdac07eb7849d3
young <aywhr75@gmail.com>
2020-02-19 07:53:02 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const exphbs  = require('express-handlebars');
+ 
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const productModel = require("./model/product");
+const productHome = require("./model/home");
+const productSell = require("./model/bestsell");
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.get('/', (req,res) =>{
+    res.render("index",{
+        title:"Home",
+        style:`home.css`,
+        home: productHome.getAllSection(),
+        bestsell:productSell. getAllSection()
+    });
+});
+
+app.get('/product', (req,res) =>{
+    res.render("product",{
+        title:"products",
+        style:`products.css`,
+        headingInfo: "All Electronics",
+        products :productModel.getAllProducts()
+    });
+});
+
+app.get('/registration', (req,res) =>{
+    res.render("registration",{
+        title:"Registration",
+        headingInfo: "Registration",
+        style:`info.css`
+    });
+});
+
+app.get('/login', (req,res) =>{
+    res.render("login",{
+        title:"Login",
+        headingInfo: "Sign-in",
+        style:`info.css`
+    });
+});
+
+app.listen(4000, () => {
+    console.log('Server is working', 4000);
+});
